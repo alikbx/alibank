15cd50b86d4128dfa6a2aa2bcc57631c39f9f2d4
Alireza rezaei <alikbx@gmail.com>
2023-11-11 15:21:51 +0330

    It's first commit!

---
+++ src/main/java/com/bank/alibank/AlibankApplication.java
@@ -0,0 +1,13 @@
+package com.bank.alibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlibankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlibankApplication.class, args);
+	}
+
+}
